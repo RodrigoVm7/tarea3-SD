9e1543db43e0f0829dbd2a78c0a379e1ae71c7f3
rodrigovm7 <rodrigovm7@gmail.com>
2020-08-10 17:59:49 -0400

    ultima version

--- mult.c
+++ mult.c
@@ -18,6 +18,7 @@ int main(int argc, char *argv[]){
 	int filasTotal=0, fil=0, col, columnasTotal;
 	int maximos[N];
 	int max;
+	int filas_calculadas, filas_faltantes;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &id_proceso);
@@ -63,53 +64,64 @@ int main(int argc, char *argv[]){
 			filasTotal++;
 		}
 
-	num_tareas = num_procesos-1;
+	num_tareas = num_procesos-1;	//	2
 	offset=0;
 
-	columnas = N/num_tareas;
+	columnas = N/num_tareas;	//	1
+
+	if(N%num_tareas==1){
+		filas_faltantes = N - columnas*num_tareas;
+	}
+
+	filas_calculadas=0;
+
+	//printf("Las filas que se calculan son:  %d\n",num_tareas*columnas);
+
 
 	for (destino=1; destino<=num_tareas;destino++){
+		if(filas_faltantes>0){
+			columnas++;
+		}
 		MPI_Send(&offset, 1, MPI_INT, destino, 1, MPI_COMM_WORLD);
 		MPI_Send(&columnas, 1, MPI_INT, destino, 1, MPI_COMM_WORLD);
 		MPI_Send(&matriz_a[offset][0], columnas*N, MPI_INT, destino, 1, MPI_COMM_WORLD);
 		MPI_Send(&matriz_b, N*N, MPI_INT, destino, 1, MPI_COMM_WORLD);
 		offset = offset + columnas;
+		filas_faltantes--;
+		if(filas_faltantes==0){
+			columnas--;
+		}
+
+	}
+
+
+	if(N%num_tareas==1){
+		filas_faltantes = N - columnas*num_tareas;
 	}
 
 	for(i=1;i<=num_tareas;i++){
+		if(filas_faltantes>0){
+			columnas++;
+		}
 		fuente=i;
 		MPI_Recv(&offset, 1, MPI_INT, fuente, 2, MPI_COMM_WORLD, &status);
 		MPI_Recv(&columnas, 1, MPI_INT, fuente, 2, MPI_COMM_WORLD, &status);
 		MPI_Recv(&matriz_c[offset][0], columnas*N, MPI_INT, fuente, 2, MPI_COMM_WORLD, &status);
 		MPI_Recv(&maximos[offset], 1, MPI_INT, fuente, 2, MPI_COMM_WORLD, &status);
-	}
-
-	printf("\n");
-	for(i=0;i<N;i++){
-		for(j=0;j<N;j++){
-			printf("[%d]",matriz_c[i][j]);
+		if(filas_faltantes==0){
+			columnas--;
 		}
-		printf("\n");
 	}
 
-	printf("El maximo es: ");
+	printf("Coeficiente mayor = ");
 	max=maximos[0];
 	for (i=1;i<N;++i){
 		if(maximos[i]>max)
 			max=maximos[i];
 	}
 	printf("%d\n",max);
-
-	//printf("\n");
-
-	/*for(i=0;i<N;i++){
-		for(j=0;j<N;j++){
-			printf("[%d]",matriz_b[i][j]);
-		}
-		printf("\n");
-	}*/
+	
 	}
-
 
 	if(id_proceso>0){
 		fuente=0;
@@ -117,7 +129,6 @@ int main(int argc, char *argv[]){
 		MPI_Recv(&columnas, 1, MPI_INT, fuente, 1, MPI_COMM_WORLD, &status);
 		MPI_Recv(&matriz_a, columnas*N, MPI_INT, fuente, 1, MPI_COMM_WORLD, &status);
 		MPI_Recv(&matriz_b, N*N, MPI_INT, fuente, 1, MPI_COMM_WORLD, &status);
-
 		for(k=0; k<N; k++){
 			for(i=0;i<columnas; i++){
 				matriz_c[i][k] = 0;
@@ -126,27 +137,26 @@ int main(int argc, char *argv[]){
 				}
 			}
 		}
-		//printf("Hola soy el proceso %d y mi numero de columnas es: %d\n",id_proceso,columnas);
-		printf("Hola soy el proceso %d y mi numero calculo es: \n",id_proceso);
+
 		max=matriz_c[0][0];
+
 		for(k=0;k<N;k++){
 			for(i=0;i<columnas;i++){
-				printf("[%d]",matriz_c[i][k]);
-				if(matriz_c[i][k]>max){
-					max=matriz_c[i][k];
+				if(matriz_c[k][i]>max){
+					max=matriz_c[k][i];
 				}
 			}
 		}
-		//printf("max: %d", max);
-		//printf("mi offset es: %d",offset);
-		//printf("mi maximo elemento es %d\n", max);
-		printf("\n");
+
 		MPI_Send(&offset, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
 		MPI_Send(&columnas, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
 		MPI_Send(&matriz_c, columnas*N, MPI_INT, 0, 2, MPI_COMM_WORLD);
 		MPI_Send(&max, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
 	}
+
 	MPI_Finalize();
+
+
 }
 
 // mpicc mult.c -o mult (COMPILAR)
